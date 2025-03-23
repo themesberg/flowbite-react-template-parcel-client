8c00f6d95a3749dc299a81c7b7f7ace5b9a629c0
SutuSebastian <sebiitv@gmail.com>
2025-03-23 17:31:26 +0200

    init template

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { App } from "./App";
+import "./App.css";
+
+let container = document.getElementById("app")!;
+let root = createRoot(container);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
