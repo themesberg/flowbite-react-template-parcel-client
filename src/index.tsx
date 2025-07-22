22dd74ea98e70c38d0cbf663cbf6db9742a696e1
Sutu Sebastian <sebiitv@gmail.com>
2025-07-22 15:27:48 +0300

    up packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { initThemeMode } from "flowbite-react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// import { ThemeInit } from "../.flowbite-react/init";
 import { App } from "./App";
 import "./App.css";
 
@@ -8,6 +9,8 @@ let container = document.getElementById("app")!;
 let root = createRoot(container);
 root.render(
   <StrictMode>
+    {/* TODO: @parcel/core: Failed to resolve 'flowbite-react/store/init' from './.flowbite-react/init.tsx' */}
+    {/* <ThemeInit /> */}
     <App />
   </StrictMode>,
 );
